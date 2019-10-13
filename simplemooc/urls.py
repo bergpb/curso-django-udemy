54ff0cf7ba73451018599505bfced23912318b42
bergpb <lindembergdepaulo@gmail.com>
2019-10-13 14:12:38 -0300

    melhorias nos templates de cursos, static e media folders

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('simplemooc.core.urls', namespace='core')),
     path('courses/', include('simplemooc.courses.urls', namespace='courses')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
