2c764c5ee89fed86852a1c545bd7989ebb6bdecb
bergpb <lindembergdepaulo@gmail.com>
2019-10-13 12:29:43 -0300

    customizaçoes nos models e admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+
 from .models import Course
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'start_date', 'created_at')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug' :('name',)}
+
+admin.site.register(Course, CourseAdmin)
